64326617ba1587f19b6276019df5d91b915bdd86
vadibaum <48681573+vadibaum@users.noreply.github.com>
2021-05-13 15:15:31 +0300

    Update bitrun.sh

--- bitrun.sh
+++ bitrun.sh
@@ -1,13 +1,15 @@
 echo "Started generation"
-./externalSort --blockgenerate $1 $2 190000000 > t$1_$2.txt
+time ./externalSort --blockgenerate $1 $2 > t$1_$2.txt
 echo "Finished generation"
 du -h t$1_$2.txt
-./externalSort --sort t$1_$2.txt t$1_$2_sorted.txt 10960000
+echo "Sorting"
+time ./externalSort --sort t$1_$2.txt t$1_$2_sorted.txt 109600000
 echo "wc run"
 wc -lwc t$1_$2.txt
 wc -lwc t$1_$2_sorted.txt
-echo "sorting by gnu sort"
-sort t$1_$2.txt > t$1_$2_sorted.2.txt
+echo "Sorting by gnu sort"
+time sort t$1_$2.txt > t$1_$2_sorted.2.txt
 wc -lwc t$1_$2_sorted.2.txt
-echo "running diffs"
+echo "Running diffs"
 diff t$1_$2_sorted.txt t$1_$2_sorted.2.txt
+rm t$1_$2.txt t$1_$2_sorted.txt t$1_$2_sorted.2.txt
